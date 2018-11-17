05ae78a3cefca49b57dd9c3494c0c1a20501e172
Norbert Aberor <38046968+norbertkross@users.noreply.github.com>
2018-11-17 19:22:11 +0000

    Add files via upload

---
+++ testOnNTP.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:ntp/ntp.dart';
+
+class TestError extends StatefulWidget {
+  _TestErrorState createState() => _TestErrorState();
+}
+
+class _TestErrorState extends State<TestError> {
+  DateTime _currentTime;
+  DateTime _ntpTime;
+  int _ntpOffset;  
+
+    void _updateTime() async {
+    _currentTime = DateTime.now();
+print("ok!");
+    NTP.getNtpOffset().then((value) {
+      print('runninf here');
+      setState(() {
+        _ntpOffset = value;
+        _ntpTime = _currentTime.add(Duration(milliseconds: _ntpOffset));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+              floatingActionButton: new FloatingActionButton(
+          tooltip: 'Update time',
+          child: const Icon(Icons.timer),
+          onPressed: _updateTime,
+        ),
+      
+       body: Container(
+         child: Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Current time: $_currentTime'),
+              Text('NTP offset:$_ntpOffset ms'),
+              Text('NTP time:$_ntpTime'),
+            ],
+          ),
+        ),
+       ),
+
+    );
+  }
+}
